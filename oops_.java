2e7843e13af94f6fe5c52d1dd7849274cc5cfaf1
Abhishek <73600362+abhiishekrana@users.noreply.github.com>
2024-06-19 11:18:20 +0530

    Add files via upload

---
+++ oops_.java
@@ -0,0 +1,19 @@
+OOPS concepts are as follows: 
+
+Class
+Object 
+Method and method passing
+Pillars of OOPs
+    1.Abstraction
+    2.Encapsulation
+    3.Inheritance
+    4.Polymorphism
+        1.Compile-time polymorphism
+        2.Runtime polymorphism
+
+Note : Why is OOPs important in Java?
+OOPs helps in organizing and structuring code in a more manageable way, making it easier to maintain and scale Java applications. It also promotes code 
+    reusability,
+    modularity, 
+    and flexibility, leading to efficient and robust software development.
+
